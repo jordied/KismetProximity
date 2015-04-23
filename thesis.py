d10832d843d7bd71c2961440f1668449fe24c367
Jordi Estivill-Dredge <j.e.dredge@gmail.com>
2015-04-23 18:37:16 +1000

    Some work on trying to break up the returned substring

--- thesis.py
+++ thesis.py
@@ -1,7 +1,22 @@
 from subprocess import Popen, PIPE
+### Functions
 
-p = Popen(['ls', '-l'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+def getClientList(text):
+	output = []
+	try:
+		text.splitlines(True)
+	except AttributeError:
+		pass
+	for line in text:
+		print line
+		if "*CLIENT" in line:
+			temp = line.split("*Client", 1)
+			output.add(temp)
+	return output
+	
+## Main
+p = Popen(['/bin/bash','-i', '-c','wlist'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 # output, err = p.communicate(b"input data that is passed to subprocess' stdin")
 output, err = p.communicate()
 rc = p.returncode
-print output
+print getClientList(output)
