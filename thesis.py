5ef6474511705f7fe1c91523c9a492f7f2702961
Jordi Estivill-Dredge <j.e.dredge@gmail.com>
2015-05-08 07:56:59 +1000

    Added it splitting up the string, but need to include new lines

--- thesis.py
+++ thesis.py
@@ -1,22 +1,39 @@
+
+import unicodedata, re
 from subprocess import Popen, PIPE
 ### Functions
 
-def getClientList(text):
+
+
+
+
+
+def remove_control_chars(s):
+        #all_chars = (unichr(i) for i in xrange(0x110000))
+        #control_chars = ''.join(c for c in all_chars if unicodedata.category(c) == 'Cc')
+        # or equivalently and much more efficiently
+        control_chars = ''.join(map(unichr, range(0,32) + range(127,160)))
+        control_char_re = re.compile('[%s]' % re.escape(control_chars))
+        return control_char_re.sub('', s)
+
+def getClientList(intext):
 	output = []
+	#print "Original Text:\n", repr(intext)
 	try:
-		text.splitlines(True)
+		intext.splitlines(True)
 	except AttributeError:
 		pass
-	for line in text:
-		print line
-		if "*CLIENT" in line:
-			temp = line.split("*Client", 1)
-			output.add(temp)
-	return output
-	
-## Main
-p = Popen(['/bin/bash','-i', '-c','wlist'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-# output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-output, err = p.communicate()
-rc = p.returncode
-print getClientList(output)
+	for line in intext:
+		# print line
+		#regex = re.compile('[\x7f\x80]');
+		text = remove_control_chars(line)
+		#print repr(text)
+		output.append(text)
+	return ''.join(output)
+
+if __name__ == "__main__":
+    p = Popen(['/bin/bash','-i', '-c','wlist'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    # output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    output, err = p.communicate()
+    rc = p.returncode
+    print "Formatted Text:", getClientList(output)
