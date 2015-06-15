915c8a645cc1511d355d0a152d57da3f65fa4a50
Jordi Estivill-Dredge <j.e.dredge@gmail.com>
2015-06-15 11:33:42 +1000

    Added a better license

---
+++ model/reciever.py
@@ -0,0 +1,46 @@
+# This file is part of Jordi Estivill-Dredge's Thesis at the University Of Queensland, known as KismetProximity.
+#
+# KismetProximity is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# KismetProximity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+__author__ = 'Jordi'
+
+import paho.mqtt.client as mqtt
+
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: "+str(rc))
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+
+def on_publish(mqttc, obj, mid):
+    print("mid: "+str(mid))
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+
+if __name__ == "__main__":
+	mqttc = mqtt.Client()
+	mqttc.on_message = on_message
+	mqttc.on_connect = on_connect
+	mqttc.on_publish = on_publish
+	mqttc.on_subscribe = on_subscribe
+	# Uncomment to enable debug messages
+	#mqttc.on_log = on_log
+	mqttc.connect('winter.ceit.uq.edu.au', 1883, 60)
+	mqttc.subscribe('proximity/#', 0)
+
+
+	mqttc.loop_forever()
