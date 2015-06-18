4ce2f7c17b9608ac03b509e969d38cd15dfb88b6
Jordi Estivill-Dredge <j.e.dredge@gmail.com>
2015-06-18 15:53:56 +1000

    A lot of work, realtime plotting with a legend and the colours aren't changing all the time.
    The window is a little larger but I suppose thats ok.

--- model/grapher.py
+++ model/grapher.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 __author__ = 'Jordi'
 import matplotlib.pyplot as plt
+from matplotlib.legend_handler import HandlerLine2D
 import numpy as np
 from multiprocessing import Process
 from datetime import datetime
@@ -23,15 +24,13 @@ import sys
 
 class Manufacterer:
 
-    def __init__(self, name, color, marker, line_style='-'):
+    def __init__(self, ax, name, color, marker, line_style='-'):
         self.name = name
-        self.color = color
-        self.marker = marker
-        self.line_style = line_style
-        self.previous_val = 0
         self.count = 0
-        self.ctime = 0
-        self.ptime = 0
+        self.x_values = [0]
+        self.y_values = [0]
+        self.line, = ax.plot(self.x_values, self.y_values, marker=marker, c=color, ms=5, ls=line_style, label=name)
+        print 'New manufacturer ({0}{1}): {2}'.format(color, marker, name)
 
 
     def increment_count(self):
@@ -41,9 +40,13 @@ class Manufacterer:
         self.previous_val = self.count
         self.count = 0
 
-    def set_time(self, time):
-        self.ptime = self.ctime
-        self.ctime = time
+    def set_data(self, time):
+        self.x_values.append(time)
+        self.y_values.append(self.count)
+        # Update the graph
+        self.line.set_xdata(np.array(self.x_values))
+        self.line.set_ydata(np.array(self.y_values))
+
 
 class Grapher:
 
@@ -59,15 +62,14 @@ class Grapher:
         self.process.daemon = True
         self.process.start()
 
-    def __set_man_style__(self, device):
+    def __set_man_style__(self, ax, device):
         already_in = False
         for val in self.devs:
             if device['man'] == val.name:
                 val.increment_count()
                 already_in = True
         if not already_in:
-            new_dev = Manufacterer(name=device['man'], color=self.colors.next(), marker=self.markers.next(), line_style=self.lines.next())
-            print 'New manufacturer ({0}{1}): {2}'.format(new_dev.color, new_dev.marker, new_dev.name)
+            new_dev = Manufacterer(ax = ax, name=device['man'], color=self.colors.next(), marker=self.markers.next(), line_style=self.lines.next())
             self.devs.append(new_dev)
 
     def __draw_a_line__(self):
@@ -75,34 +77,41 @@ class Grapher:
             sys.stdout.write('-')
         sys.stdout.write('\n')
 
-    def __draw_points__(self, tdiff, devices):
+    def __draw_points__(self, ax, tdiff, devices):
         y_max = 1
-        self.__draw_a_line__()
+        #self.__draw_a_line__()
         for y, dev in enumerate(devices):
-            self.__set_man_style__(dev)
+            self.__set_man_style__(ax, dev)
         for val in self.devs:
             if val.count > y_max:
                 y_max = val.count + 1
-            val.set_time(tdiff.seconds)
-            print "Found {0} {1} devices".format(val.count, val.name)
-            x = np.array([val.ptime, val.ctime])
-            y = np.array([val.previous_val, val.count])
-            plt.plot(x, y, marker=val.marker, c=val.color, ms=5, ls=val.line_style)
+            val.set_data(tdiff.seconds)
+            #print "Found {0} {1} devices".format(val.count, val.name)
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        leg = plt.gca().get_legend()
+        plt.setp(leg.get_texts(), fontsize='small')
         return y_max
 
     def plot_a_graph(self):
         start_time = datetime.now()
         # Set initial values
+        fig = plt.figure(num=None, figsize=(16, 8), dpi=80)
+        ax = plt.subplot(111)
         y = 0
         y_max = 5
         # Setup Plot
-        plt.axis([0, 2, 0, y_max])
+        ax.axis([0, 2, 0, y_max])
+        plt.xlabel('Time (s)')
+        plt.ylabel('Device Count')
+        plt.title('Number of Active Devices Detected')
         plt.ion()
         plt.show()
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
         while True:
             try:
                 msg = self.queue.get(False)
-                y = self.__draw_points__(tdiff, msg)
+                y = self.__draw_points__(ax, tdiff, msg)
                 for x in self.devs:
                     x.reset_count()
             except Empty:
@@ -111,7 +120,6 @@ class Grapher:
             # Increase height if necessary
             if y > y_max:
                 y_max = y + 1
-            plt.axis([0, tdiff.seconds, 0, y_max])
-            plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+            ax.axis([0, tdiff.seconds, 0, y_max])
             plt.draw()
             time.sleep(0.5)
