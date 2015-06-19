ff88fb8e52b2702463c08762aebb911f4398e8a4
Jordi Estivill-Dredge <j.e.dredge@gmail.com>
2015-06-19 15:43:50 +1000

    Working on adding the manual counting functionality.

--- model/reciever.py
+++ model/reciever.py
@@ -22,7 +22,7 @@ from datetime import datetime, timedelta
 from Queue import Empty
 import time
 import itertools
-import sys
+import csv
 
 import dicttoxml
 from lxml import etree
@@ -31,9 +31,67 @@ import paho.mqtt.client as mqtt
 import matplotlib.pyplot as plt
 import numpy as np
 
+class CSVLogger:
+    def __init__(self, rec_queue, filename):
+        self.rec_queue = rec_queue
+        self.man_list = []
+        self.__file_string__ = 'logs/log_{0}.csv'.format(datetime.now().strftime("%H_%M_%S_%B_%d_%Y"))
+        f = open(self.__file_string__, 'wb')
+        self.csv_writer = csv.writer(f, delimiter=',')
+
+
+
+class UserInputMonitor:
+
+    def __init__(self, queue, logger):
+        self.logger = logger
+        self.ui_queue = queue
+        self.person_count = 0
+        # Process
+        self.process = Process(target=self.user_input)
+        self.process.daemon = True
+        self.process.start()
+
+    def user_input(self):
+        while True:
+            self.print_person_count()
+            self.get_ui()
+            self.put_value()
+
+    def print_person_count(self):
+        msg = 'Person count is now {0}'.format(self.person_count)
+        self.logger.info(msg)
+        print msg
+
+    def put_value(self):
+        self.ui_queue.put_nowait(self.person_count)
+
+    def get_ui(self):
+        response = raw_input("Enter Person Count: ")
+        if len(response) > 1:
+            if response[0] == '+':
+                try:
+                    self.person_count += int((response.split('+'))[1])
+                except:
+                    pass
+            if response[0] == '-':
+                try:
+                    self.person_count -= int((response.split('-'))[1])
+                    if self.person_count < 0:
+                        self.person_count = 0
+                except:
+                    pass
+            if response[0] == '=':
+                try:
+                    self.person_count = int((response.split('='))[1])
+                except:
+                    pass
+
+
+
 
 class Receiver:
-    def __init__(self, timeout, filename, queue, logger):
+    def __init__(self, timeout, filename, send_queue, logger):
         self.logger = logger
         self.timeout = timeout
         self.current_time = datetime.now()
@@ -41,7 +99,7 @@ class Receiver:
         self.time_strings = ['time', 'rssi', 'pi_id']
         self.pp = pprint.PrettyPrinter(indent=4)
         self.file = open(filename, "wb")
-        self.queue = queue
+        self.send_queue = send_queue
 
     def __print_device__(self, adding, device):
         if adding:
@@ -57,10 +115,6 @@ class Receiver:
         except KeyError:
             man = 'UNKNOWN-MAN'
         self.logger.info("{0}\t{1}\t{2}".format(status, mac, man))
-
-    def __writer__(self, msg):
-        ## Write to the queue
-        self.queue.put(msg)  # Write 'count' numbers into the queue
 
     def on_connect(self, mqttc, obj, flags, rc):
         self.logger.debug("Connected! - " + str(rc))
@@ -96,7 +150,7 @@ class Receiver:
         tree = etree.fromstring(tmp_xml, etree.XMLParser())
         tree.set('id', datetime.now().strftime(format))
         self.file.write(etree.tostring(tree, pretty_print=True))
-        self.queue.put_nowait(self.devices)
+        self.send_queue.put_nowait(self.devices)
 
     def on_publish(self, mqttc, obj, mid):
         self.logger.debug("Published! " + str(mid))
@@ -108,8 +162,8 @@ class Receiver:
         self.logger.info(string)
 
 
-def listener(queue, args, logger):
-    receiver = Receiver(timeout=args.timeout, filename=args.file, queue=queue, logger=logger)
+def listener(send_queue, args, logger):
+    receiver = Receiver(timeout=args.timeout, filename=args.file, send_queue=send_queue, logger=logger)
     mqttc = mqtt.Client()
     mqttc.on_message = receiver.on_message
     mqttc.on_connect = receiver.on_connect
@@ -151,9 +205,10 @@ class Manufacterer:
 
 
 class Grapher:
-    def __init__(self, queue, logger, period):
+    def __init__(self, queue, ui_queue, logger, period):
         self.logger = logger
         self.queue = queue
+        self.ui_queue = ui_queue
         self.markers = itertools.cycle(('x', '+', '.', 'o', '*'))
         self.colors = itertools.cycle(('b', 'g', 'r', 'm', 'y', 'k', 'Aqua', 'Chocolate', 'DeepPink', 'Lime', 'Purple'))
         self.lines = itertools.cycle((':', '-.', '--'))
@@ -175,7 +230,7 @@ class Grapher:
                                    line_style=self.lines.next())
             self.devs.append(new_dev)
 
-    def __draw_points__(self, ax, tdiff, devices):
+    def __draw_device_points__(self, ax, tdiff, devices):
         y_max = 1
         for y, dev in enumerate(devices):
             self.__set_man_style__(ax, dev)
@@ -184,10 +239,21 @@ class Grapher:
                 y_max = val.count + 1
             val.set_data(tdiff.seconds)
             # print "Found {0} {1} devices".format(val.count, val.name)
+        return y_max
+
+    def __draw_ui_point__(self, ax, tdiff, count, man_count):
+        y_max = 1
+        if man_count.count > y_max:
+            y_max = man_count.count + 1
+        man_count.count = count
+        man_count.set_data(tdiff.seconds)
+        # print "Found {0} {1} devices".format(val.count, val.name)
+        return y_max
+
+    def __draw_legend__(self, ax):
         ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         leg = plt.gca().get_legend()
         plt.setp(leg.get_texts(), fontsize='small')
-        return y_max
 
     def plot_a_graph(self):
         start_time = datetime.now()
@@ -195,6 +261,7 @@ class Grapher:
         fig = plt.figure(num=None, figsize=(16, 8), dpi=80)
         ax = plt.subplot(111)
         y = 0
+        z = 0
         y_max = 5
         # Setup Plot
         ax.axis([0, self.period, 0, y_max])
@@ -205,18 +272,33 @@ class Grapher:
         plt.show()
         box = ax.get_position()
         ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        # Create UI Manufacterer
+        man_count = Manufacterer(logger=logging.getLogger('UI'), ax=ax, name='Manual_Count', color='r', marker='x',
+                                   line_style='-')
         while True:
+            # Get data from MQTT
             try:
                 msg = self.queue.get(False)
-                y = self.__draw_points__(ax, tdiff, msg)
+                y = self.__draw_device_points__(ax, tdiff, msg)
                 for x in self.devs:
                     x.reset_count()
             except Empty:
-                msg = None
+                pass
+            # Try and get from UI
+            try:
+                ui = self.ui_queue.get(False)
+                z = self.__draw_ui_point__(ax, tdiff, ui, man_count)
+            except Empty:
+                pass
+            self.__draw_legend__(ax)
+            # Get ready to plot
             tdiff = datetime.now() - start_time
             # Increase height if necessary
             if y > y_max:
                 y_max = y + 1
+            if z > y_max:
+                y_max = z + 1
+            # Update the graph and show
             ax.axis([0, tdiff.seconds + self.period, 0, y_max])
             plt.draw()
             time.sleep(self.period)
@@ -244,10 +326,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
     logging.basicConfig(filename=args.logfile, format='%(asctime)-15s::%(levelname)s:: %(message)s', level=args.loglevel)
     # Create a Multi Process Queue
-    queue = multiprocessing.Queue()
+    MQTT_to_Graph = multiprocessing.Queue()
+    UI_to_MQTT = multiprocessing.Queue()
     # Create a graphing process
     if args.graph:
-        grapher = Grapher(queue, logging.getLogger('Graph'), (1/float(args.freq)))
+        grapher = Grapher(MQTT_to_Graph, UI_to_MQTT, logging.getLogger('Graph'), (1/float(args.freq)))
         # MQTT process
-    listener(queue, args, logging.getLogger('MQTT_Listener'))
+    listener(MQTT_to_Graph, args, logging.getLogger('MQTT_Listener'))
 
