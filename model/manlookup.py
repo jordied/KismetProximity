5f99e178b478b7714cc49da4813e583435d8ab3c
Jordi Estivill-Dredge <j.e.dredge@gmail.com>
2015-11-05 20:44:51 +1000

    Supporting evidence for the document

---
+++ model/manlookup.py
@@ -0,0 +1,24 @@
+# This file is part of Jordi Estivill-Dredge's Thesis at the University Of Queensland, known as KismetProximity.
+#
+# KismetProximity is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# KismetProximity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+__author__ = 'Jordi'
+
+from netaddr import *
+import sys
+
+try:
+    mac = sys.argv[1]
+except IndexError:
+    print 'Please add a MAC Address'
+    exit(-1)
+print EUI(mac).oui.registration().org
